4dfe706905fe30993e534860de5fe15c93ef5fa7
Filip Nilsson <filip.n@outlook.com>
2023-10-22 17:07:58 +0200

    Adjusted some comments

--- Employee.cs
+++ Employee.cs
@@ -8,12 +8,14 @@ namespace OOP_Generic_collections
 {
     internal class Employee
     {
+        // Declaration of class properties
         public int Id { get; set; }
         public string Name { get; set; }
         public string Gender { get; set; }
         public double Salary { get; set; }
 
 
+        // Class constructor
         public Employee(int id, string name, string gender, double salary)
         {
             Id = id;
@@ -23,8 +25,7 @@ namespace OOP_Generic_collections
         }
 
 
-
-        // Used to override the build in method of 
+        // Used to override the build in ToString() method in the Employee class to return a custom string
         public override string ToString()
         {
             return $"{Id} - {Name} - {Gender} - {Salary}";
